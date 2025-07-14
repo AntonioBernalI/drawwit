200985b1bb45a4ae191500189ad989abceb7d8f3
Antonio <a01772490@tec.mx>
2025-07-13 20:23:48 -0600

    added homescreen onto a component

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { Devvit, useState, svg } from '@devvit/public-api';
-
+import HomeScreen from "./components/HomeScreen.jsx";
 Devvit.configure({
   redditAPI: true,
 });
@@ -34,60 +34,9 @@ Devvit.addCustomPostType({
   height: 'regular',
   render: (context) => {
     const [counter, setCounter] = useState(0);
-
     return (
-        <vstack height={"100%"} width={"100%"} backgroundColor={"#f0f0f0"} alignment={"center middle"} borderColor={"#000"} border={"thick"}>
-          <hstack width={"100%"} height={"30%"} alignment={"bottom center"}>
-            <hstack width={"100%"} height={"100%"}>
-              <image
-                  width={"100%"}
-                  height={"100%"}
-                  url={"Drawwit.png"}
-                  imageWidth={622}
-                  imageHeight={226}
-              />
-            </hstack>
-          </hstack>
-          <hstack width={"100%"} height={"35%"} alignment={"bottom center"}>
-            <hstack width={"60%"} height={"60%"} backgroundColor={"#fff"} cornerRadius={"medium"} alignment={"center middle"} borderColor={"#000"} border={"thick"}>
-              <hstack width={"90%"} height={"90%"}>
-                <image
-                    width={"100%"}
-                    height={"100%"}
-                    url={"CreateContest.png"}
-                    imageWidth={378}
-                    imageHeight={60}
-                />
-              </hstack>
-            </hstack>
-          </hstack>
-          <hstack width={"100%"} height={"35%"} alignment={"center middle"}>
-            <hstack width={"45%"} height={"65%"} backgroundColor={"#fff"} cornerRadius={"medium"} borderColor={"#000"} border={"thick"} alignment={"middle center"}>
-              <hstack width={"90%"} height={"90%"}>
-                <image
-                    width={"100%"}
-                    height={"100%"}
-                    url={"HowToPlay.png"}
-                    imageWidth={1134}
-                    imageHeight={836}
-                />
-              </hstack>
-            </hstack>
-            <spacer></spacer>
-            <hstack width={"45%"} height={"65%"} backgroundColor={"#fff"} cornerRadius={"medium"} borderColor={"#000"} border={"thick"} alignment={"middle center"}>
-              <hstack width={"90%"} height={"90%"}>
-                <image
-                    width={"100%"}
-                    height={"100%"}
-                    url={"ViewGlobalLeaderboard.png"}
-                    imageWidth={1987}
-                    imageHeight={836}
-                />
-              </hstack>
-            </hstack>
-          </hstack>
-        </vstack>
-    );
+        <HomeScreen/>
+    )
   },
 });
 
